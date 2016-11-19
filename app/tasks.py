06d75e8986bb5824e95900b78ff97f1ac087afe6
Justin Luthy <Justin@Justins-MacBook-Pro.local>
2016-11-19 15:55:33 -0800

    caching bug fix

--- app/tasks.py
+++ app/tasks.py
@@ -1,5 +1,5 @@
 from app import app, cli, redis_cache, celery
-
+from flask import jsonify
 
 
 
@@ -12,6 +12,6 @@ def cache_urls(urls):
       not_cached_urls.append('u')
   resp = cli.oembed(not_cached_urls, raw = True, words=30)
   for i in resp:
-      c = redis_cache.set(i["original_url"], i["raw"])
+      c = redis_cache.set(i["original_url"], jsonify(i['raw']))
       print 'Item cached in Redis', c
   return True
