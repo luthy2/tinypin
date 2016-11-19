247165789f0a1e0f1bb91793202533cc6aa9cbaa
Justin Luthy <Justin@Justins-MacBook-Pro.local>
2016-11-19 14:55:34 -0800

    caching bug fix

--- app/client.py
+++ app/client.py
@@ -16,7 +16,7 @@ def get_content(url):
         print type(resp), "item loaded from cache"
     else:
         resp = cli.oembed(url, words = 30)
-        redis_cache.set(url, resp)
+        redis_cache.set(url, resp.json())
     print resp
     if resp.get("provider_name") == "Twitter":
         return render_twitter(url)
