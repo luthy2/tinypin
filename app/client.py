d774d1d1fb4b9da7f05cfed592ab921ab1f0fdba
Justin Luthy <Justin@Justins-MacBook-Pro.local>
2016-12-05 14:50:20 -0800

    fix for embedly error

--- app/client.py
+++ app/client.py
@@ -22,29 +22,32 @@ def get_content(url):
             r = redis_cache.set(url, resp['raw'])
             print 'item cached:', r
     print resp
-    if resp.get("provider_name") == "Twitter":
-        return render_twitter(url)
-    elif resp.get("provider_name") == "YouTube":
-        return render_youtube(resp.get("html"))
-    elif resp["type"] == "rich":
-        print "rich"
-        ratio = (float(resp.get("height",1))/resp.get("width",1))*100
-        print ratio
-        if ratio <= 0:
-            ratio = 100
-        return render_template("video.html", content = resp.get("html"), ratio = str(ratio))
-    elif resp["type"] == "video":
-        print "video"
-        ratio = (float(resp.get("height", 1))/resp.get("width",1))*100
-        print ratio
-        if ratio <= 0:
-            ratio = 100
-        return render_template("video.html", content = resp.get("html"), ratio = ratio)
-    elif resp["type"] == "link":
-        return render_template("article.html", title = resp.get("title"), image=resp.get("thumbnail_url"), description = resp.get("description"), _url=resp.get("url"), provider = resp.get("provider_name"))
-    elif resp["type"]  == "photo":
-        print "photo"
-        return render_template("photo.html", _url = str(resp["url"]), source = resp.original_url)
+    if not resp.get('error'):
+        if resp.get("provider_name") == "Twitter":
+            return render_twitter(url)
+        elif resp.get("provider_name") == "YouTube":
+            return render_youtube(resp.get("html"))
+        elif resp["type"] == "rich":
+            print "rich"
+            ratio = (float(resp.get("height",1))/resp.get("width",1))*100
+            print ratio
+            if ratio <= 0:
+                ratio = 100
+            return render_template("video.html", content = resp.get("html"), ratio = str(ratio))
+        elif resp["type"] == "video":
+            print "video"
+            ratio = (float(resp.get("height", 1))/resp.get("width",1))*100
+            print ratio
+            if ratio <= 0:
+                ratio = 100
+            return render_template("video.html", content = resp.get("html"), ratio = ratio)
+        elif resp["type"] == "link":
+            return render_template("article.html", title = resp.get("title"), image=resp.get("thumbnail_url"), description = resp.get("description"), _url=resp.get("url"), provider = resp.get("provider_name"))
+        elif resp["type"]  == "photo":
+            print "photo"
+            return render_template("photo.html", _url = str(resp["url"]), source = resp.original_url)
+        else:
+            return render_nostyle(url)
     else:
         return render_nostyle(url)
 
